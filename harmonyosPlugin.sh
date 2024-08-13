e5821ed7e781459f4854a59b43875629291ad6e8
Yoram 曾院全 <ZENGYQ3@cn.midea.com>
2024-08-13 09:44:01 +0800

    添加插件npm清除本地缓存配置

--- harmonyosPlugin.sh
+++ harmonyosPlugin.sh
@@ -1,3 +1,5 @@
 #事业部鸿蒙npm插件包配置信息(如下配置示例)
 #rm -rf "./src/modules/module_plugin/T0x13"
 #  echo "文件夹【module_plugin/T0x13】删除成功！"
+rm -rf "./src/modules/module_plugin/T0xBX
+echo "文件夹【module_plugin/T0xBX】删除成功！
