731c3a9d895ee093813917037b3df46ef62d80ce
叶智豪 <ex_yezh11@partner.midea.com>
2024-08-22 13:52:56 +0800

    升级弹窗调用

--- pages/index/index.js
+++ pages/index/index.js
@@ -2270,6 +2270,7 @@ Page({
     //处理websocket相关逻辑
     console.log('优化 onload', dateFormat(new Date(), 'yyyy-MM-dd hh:mm:ss.S'))
     trackLoaded('page_loaded_event', 'pageOnLoad')
+    this.checkVersionUpdate()
     //this.initPushData()
     currentPageOptions = options
     var self = this
